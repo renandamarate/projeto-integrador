98e1dc45e3845cd3bdd9955e9060a74addaf411e
renandamarate <damarates@hotmail.com>
2018-10-25 20:43:38 -0300

    alegria pessoal

--- home.php
+++ home.php
@@ -4,10 +4,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Estude para o Enade</title>
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+        crossorigin="anonymous">
 </head>
-<body background="https://scontent.fbfh2-1.fna.fbcdn.net/v/t1.15752-9/44755109_371678940041290_6470060823434756096_n.png?_nc_cat=108&_nc_ht=scontent.fbfh2-1.fna&oh=f7cedb73845cbf44fca7cea9f26daa45&oe=5C3FAB2F">
 
-
+<body background="images/background.png">
+    <style>
+        img {
+            height: 300px;
+            width: 300px;
+        }
+        img:hover {
+            transition: all .2s ease-in-out;
+            transform: scale(1.1);
+        }
+    </style>
+    <div style="text-align: center">
+        <img src="images/1.png" class="img-fluid " alt="1" />
+        <img src="images/2.png" class="img-fluid " alt="2" />
+        <img src="images/3.png" alt="3" />
+    </div>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
